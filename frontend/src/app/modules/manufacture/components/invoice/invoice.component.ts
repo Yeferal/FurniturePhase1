d6a68eca6fee3d84d183ad6e50f9ef4114a3ca6d
Carlos Soberanis <jcsru13@gmail.com>
2022-02-25 22:38:06 -0600

    Add: invoice html estructure and little logic behind

--- frontend/src/app/modules/manufacture/components/invoice/invoice.component.ts
+++ frontend/src/app/modules/manufacture/components/invoice/invoice.component.ts
@@ -7,9 +7,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class InvoiceComponent implements OnInit {
 
+  furnitures: Array<{id: number, description: string}>;
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  addFurniture(id: number, description: string){
+    this.furnitures.push({id: id,description: description});
+  }
+
+  removeFurniture(id: number){
+    for(let i = 0; i < this.furnitures.length; i++){
+      if(this.furnitures[i].id == id) this.furnitures.splice(i,1);
+    }
+  }
+
+  doInvoice(){
+    console.log('-------------------------------------------------------')
+    console.log('Se ha generado una factura con los siguientes muebles: ');
+    console.log({'furnitures in invoice': this.furnitures});
+    console.log('-------------------------------------------------------')
+  }
 }
