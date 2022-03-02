0eb414143ebc1a6bf6fe71997eb5dbe4e92d6c44
Carlos Soberanis <jcsru13@gmail.com>
2022-03-02 16:54:38 -0600

    Fix: arreglando cambio de tabla dinámica

--- frontend/src/app/modules/admin/components/report-table/report-table.component.ts
+++ frontend/src/app/modules/admin/components/report-table/report-table.component.ts
@@ -12,39 +12,39 @@ export class ReportTableComponent implements OnInit {
   items: Array<any> = [
     {
       value1: 'id 1',
-      value2: 'valor 2',
-      value3: 'valor 3',
-      value4: 'valor 4',
-      value5: 'valor 5',
-      value6: 'valor 6'
+      value2: 'valor 1',
+      value3: 'valor 1',
+      value4: 'valor 1',
+      value5: 'valor 1',
+      value6: 'valor 1'
     },{
-      value1: 'id 1',
+      value1: 'id 2',
       value2: 'valor 2',
-      value3: 'valor 3',
-      value4: 'valor 4',
-      value5: 'valor 5',
-      value6: 'valor 6'
+      value3: 'valor 2',
+      value4: 'valor 2',
+      value5: 'valor 2',
+      value6: 'valor 2'
     },{
-      value1: 'id 1',
-      value2: 'valor 2',
+      value1: 'id 3',
+      value2: 'valor 3',
       value3: 'valor 3',
-      value4: 'valor 4',
-      value5: 'valor 5',
-      value6: 'valor 6'
+      value4: 'valor 3',
+      value5: 'valor 3',
+      value6: 'valor 3'
     },{
-      value1: 'id 1',
-      value2: 'valor 2',
-      value3: 'valor 3',
+      value1: 'id 4',
+      value2: 'valor 4',
+      value3: 'valor 4',
       value4: 'valor 4',
-      value5: 'valor 5',
-      value6: 'valor 6'
+      value5: 'valor 4',
+      value6: 'valor 4'
     },{
-      value1: 'id 1',
-      value2: 'valor 2',
-      value3: 'valor 3',
-      value4: 'valor 4',
+      value1: 'id 5',
+      value2: 'valor 5',
+      value3: 'valor 5',
+      value4: 'valor 5',
       value5: 'valor 5',
-      value6: 'valor 6'
+      value6: 'valor 5'
     }
   ];
   @Input() set reportType(value: number){ //Cada vez que se setee el reportType
