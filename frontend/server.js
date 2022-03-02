179f86150df93f3642d740e99a1847aef59232dd
Yeferal <yefercuber20@gmail.com>
2022-03-02 10:11:27 -0600

    update

--- frontend/server.js
+++ frontend/server.js
@@ -1,14 +1,15 @@
-const path = require('path');
+//Install express server
 const express = require('express');
+const path = require('path');
+
 const app = express();
 
-// Serve static files
-app.use(express.static(__dirname + '/dist/frontend'));
+// Serve only the static files form the dist directory
+app.use(express.static('./dist/frontend'));
 
-// Send all requests to index.html
-app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + '/dist/frontend/index.html'));
-});
+app.get('/*', (req, res) =>
+    res.sendFile('index.html', {root: 'dist/frontend/'}),
+);
 
-// default Heroku port
-app.listen(process.env.PORT || 5000);
+// Start the app by listening on the default Heroku port
+app.listen(process.env.PORT || 8080);
