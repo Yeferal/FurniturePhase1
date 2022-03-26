e3d3288006daeeba8ba1deb24b182649d29f2ece
Yeferal <yefercuber20@gmail.com>
2022-03-26 16:56:37 -0600

    add form report

--- frontend/src/app/modules/admin/components/report-table/report-table.component.ts
+++ frontend/src/app/modules/admin/components/report-table/report-table.component.ts
@@ -10,51 +10,12 @@ export class ReportTableComponent implements OnInit {
 
   _reportType: number;
   p: number = 1;
-<<<<<<< HEAD
   items: Array<any> = [];
 
   @Input() set listItems(value: Array<any>){
     if(value != undefined){
       console.log("Se ha recibido el listado:" + value);
       this.items = value;
-=======
-  items: Array<any> = [
-    {
-      value1: 'ids 1',
-      value2: 'valor 1',
-      value3: 'valor 1',
-      value4: 'valor 1',
-      value5: 'valor 1',
-      value6: 'valor 1'
-    },{
-      value1: 'id 2',
-      value2: 'valor 2',
-      value3: 'valor 2',
-      value4: 'valor 2',
-      value5: 'valor 2',
-      value6: 'valor 2'
-    },{
-      value1: 'id 3',
-      value2: 'valor 3',
-      value3: 'valor 3',
-      value4: 'valor 3',
-      value5: 'valor 3',
-      value6: 'valor 3'
-    },{
-      value1: 'id 4',
-      value2: 'valor 4',
-      value3: 'valor 4',
-      value4: 'valor 4',
-      value5: 'valor 4',
-      value6: 'valor 4'
-    },{
-      value1: 'id 5',
-      value2: 'valor 5',
-      value3: 'valor 5',
-      value4: 'valor 5',
-      value5: 'valor 5',
-      value6: 'valor 5'
->>>>>>> 9ddc20d90c698a7d1cdf2fed338ea0d6e1b07ce5
     }
   }
 
