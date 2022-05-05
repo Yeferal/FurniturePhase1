333e5478a7785c64f56a8fff09a1e9222605584a
Yeferal <yefercuber20@gmail.com>
2022-05-05 17:12:23 -0600

    add deploy for test

--- frontend/src/app/core/rutas.ts
+++ frontend/src/app/core/rutas.ts
@@ -1,8 +1,14 @@
 export const GLOBAL ={
-    INVENTORY_SERVICE : "http://localhost:8083/",
-    ADMIN_SERVICE : "http://localhost:8081/",
-    FURNITURE_SERVICE : "http://localhost:8082/",
-    REPORT_SERVICE : "http://localhost:8084/",
-    SALE_SERVICE : "http://localhost:8085/",
-    SALES_REPORT_SERVICE : "http://localhost:8086/"
+    // INVENTORY_SERVICE : "http://localhost:8083/",
+    // ADMIN_SERVICE : "http://localhost:8081/",
+    // FURNITURE_SERVICE : "http://localhost:8082/",
+    // REPORT_SERVICE : "http://localhost:8084/",
+    // SALE_SERVICE : "http://localhost:8085/",
+    // SALES_REPORT_SERVICE : "http://localhost:8086/"
+    INVENTORY_SERVICE : "http://147.182.223.39:8083/",
+    ADMIN_SERVICE : "http://147.182.223.39:8081/",
+    FURNITURE_SERVICE : "http://147.182.223.39:8082/",
+    REPORT_SERVICE : "http://147.182.223.39:8084/",
+    SALE_SERVICE : "http://147.182.223.39:8085/",
+    SALES_REPORT_SERVICE : "http://147.182.223.39:8086/"
 }
